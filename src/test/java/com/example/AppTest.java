c9494badcc8aecd4d6ea929824eae4e4b9e6ffb6
MaXimeHH <116813482+MaXimeHH@users.noreply.github.com>
2024-02-08 08:56:48 +0100

    Update AppTest.java

--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -18,6 +18,6 @@ public class AppTest {
     @Test
     public void testAppMessage() {
         App app = new App();
-        assertEquals("Hello Worlddd!", app.getMessage());
+        assertEquals("Hello World!", app.getMessage());
     }
 }
